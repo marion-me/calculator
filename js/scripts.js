a034997f95071a681d0b16334ba81dfefbb834fd
marion vutagwa <marionvutagwa@yahoo.com>
2020-02-12 20:35:11 +0300

    Add js/scripts.js

---
+++ js/scripts.js
@@ -0,0 +1,14 @@
+var add = function(number1, number2){
+    return number1 + number2;
+    };
+    var subtruct = function(number1, number2){
+    return number1-number2;
+    };
+    alert(subtruct(10, 5))
+    var add = function(number1, number2){
+    return number1 + number2;
+    };
+    var number1 = parseInt(prompt("enter a number:"));
+    var number2 = parseInt(prompt("enter another number:"));
+    alert(add(number1, number2));
+    
